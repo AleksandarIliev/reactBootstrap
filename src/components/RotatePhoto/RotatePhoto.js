3f9d6af40201de9e0b28c93e4f64ee9dda037e22
Aleksandar Iliev <aleksandar.m.iliev@abv.bg>
2023-04-30 19:02:50 +0300

    Fix jpeg and a little code

--- src/components/RotatePhoto/RotatePhoto.js
+++ src/components/RotatePhoto/RotatePhoto.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import './rotatedPhoto.module.css';
 
 export default function ControlledCarousel() {
     const [index, setIndex] = useState(0);
@@ -9,44 +10,47 @@ export default function ControlledCarousel() {
     };
 
     return (
-    <div>
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={require(`./Big-Bag-station.jpg`)}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>Big bag station</h3>
-                    {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={require(`./Bunker.jpg`)}
-                    alt="Second slide"
-                />
+        <div>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <img
+                        height={'600px'}
+                        className="d-block w-100"
+                        src={require(`./Big-Bag-station.jpg`)}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3><a href='/products'>Big bag station</a></h3>
+                        {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        height={'600px'}
+                        className="d-block w-100"
+                        src={require(`./Bunker.jpg`)}
+                        alt="Second slide"
+                    />
 
-                <Carousel.Caption>
-                    <h3>Bunker</h3>
-                    {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={require(`./Conveying-belt.jpg`)}
-                    alt="Third slide"
-                />
+                    <Carousel.Caption>
+                        <h3><a href='/products'>Bunker</a></h3>
+                        {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        height={'600px'}
+                        className="d-block w-100"
+                        src={require(`./Conveying-belt.jpg`)}
+                        alt="Third slide"
+                    />
 
-                <Carousel.Caption>
-                    <h3>Conveying belt</h3>
-                    {/* <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+                    <Carousel.Caption>
+                        <h3><a href='/products'>Conveying belt</a></h3>
+                        {/* <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 }
